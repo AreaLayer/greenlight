2d307f95827f6c265b472447fbbf29f679509382
Christian Decker <decker.christian@gmail.com>
2022-11-16 14:46:00 +0100

    plugin: Extract signatures and payloads from requests
    These will be passed to the signer in order to prove authenticity of
    the commands, before reconciling state changes with pending commands.

--- libs/gl-signerproxy/src/pb.rs
+++ libs/gl-signerproxy/src/pb.rs
@@ -13,9 +13,10 @@ impl HsmRequestContext {
         let dbid = BigEndian::read_u64(&msg.body[35..43]);
         let caps = BigEndian::read_u64(&msg.body[43..51]);
         Ok(HsmRequestContext {
+            dbid,
             node_id: node_id.to_vec(),
-            dbid: dbid,
             capabilities: caps,
+            requests: Vec::new(),
         })
     }
 }
